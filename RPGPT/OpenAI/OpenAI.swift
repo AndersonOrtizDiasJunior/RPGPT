c264b8453cb896e134098cdc241f3c69c3b8e652
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2023-04-27 10:30:42 -0300

    style: moving strings to constants file

--- RPGPT/OpenAI/OpenAI.swift
+++ RPGPT/OpenAI/OpenAI.swift
@@ -5,10 +5,7 @@ import Combine
 final class OpenAI {
 
     private var client: OpenAISwift?
-    private var chatHistory: [ChatMessage] = [
-        ChatMessage(role: .system, content: "You are a tabletop RPG dungeon master."),
-        ChatMessage(role: .user, content: "Hello, Chat GPT! I would like to play an RPG game and I was wondering if you could be my Dungeon Master. Can you create a game setting for me and guide me through the adventure? Also, can you let me know when I should roll a d20 for my actions? You can start by asking me to create a character and letting me describe it and its name"),
-    ]
+    private var chatHistory: [ChatMessage] = AppConstants.initialChat
 
     func setup(apiKey: String) {
         client = OpenAISwift(authToken: apiKey)
